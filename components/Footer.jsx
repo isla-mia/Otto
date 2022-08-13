75405d97f89b77f6ec9e6d0d3ca03e8381019f36
isla-mia <mario@isla-miablockchain.solutions>
2022-08-13 09:41:34 -0400

    Committed to Otto repository

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,8 +8,9 @@ import Button from './Button';
 const FooterLinks = ({ heading, items, extraClasses }) => (
   <div className={`flex-1 justify-start items-start ${extraClasses}`}>
     <h3 className="font-roboto dark:text-white text-nft-black-1 font-semibold text-xl mb-10">{heading}</h3>
-    {items.map((item, index) => <p key={item + index} className="font-roboto dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3">
-    {item === 'Explore' ? }</p>)}
+    {items.map((item, index) => (
+      <p key={item + index} className="font-roboto dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3" />
+    ))}
   </div>
 );
 
